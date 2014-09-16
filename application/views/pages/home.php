1b17040c78c06ea1f4b081880328fe2c708f6cae
kampoengboy <mike@aMichaels-MacBook-Air.local>
2014-09-16 22:13:44 +0700

    Add Kolexia HomePage

---
+++ application/views/pages/home.php
@@ -0,0 +1,18 @@
+<!--Home Page Content-->
+
+<div id="content">
+	<img src="images/sandal.jpg" width="50%" height="65%" style="padding-top:5px" alt="imagesandal" class="sandal" />
+	<div class="circle">
+		<img src="images/face.jpg" width="25%" height="20%" style="float:left; margin-top:-35%; margin-left:60%" class="face" />	
+	</div>
+	<p id="desc-face" style="float:right; font-size:20px; font-family:Georgia, Verdana, Times New Roman; margin-top:-20%; margin-right:25%">Jessie</p>
+	<p id="desc-face" style="float:right; font-size:20px; font-family:Georgia, Verdana, Times New Roman; margin-top:-16%; margin-right:22.5%">BooBoo Shop</p>
+	<table class="list-item" style="float:right; margin-top:-10%; margin-right:120px">
+		<tr>
+			<td class="list1"><img src="images/1.jpg" width="100px" height="100px"></td>
+			<td class="list2"><img src="images/2.jpg" width="100px" height="100px"></td>
+			<td class="list3"><img src="images/3.jpg" width="100px" height="100px"></td>
+			<td class="list4"><img src="images/4.jpg" width="100px" height="100px"></td>
+		</tr>
+	</table>
+</div>
---
+++ application/views/templates/header.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+	<title>Kolexia | <?php echo $title ?></title>
+	<link rel="stylesheet" type="text/css" href="home.css">
+	
+</head>
+<body>
+<div id="pu">
+	<div class= "nav-pu">
+		<h1 class="logo-name">Kolexia</h1>
+	</div>
+</div>
