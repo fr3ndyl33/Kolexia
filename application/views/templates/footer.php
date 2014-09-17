298d9cf6a87a7fc9c80398e0fbf699e082dffcab
kampoengboy <mike@aMichaels-MacBook-Air.local>
2014-09-17 12:52:23 +0700

    Fix Problem

---
+++ application/views/templates/footer.php
@@ -0,0 +1,44 @@
+<hr class="pd-line">
+<div id = "pd">
+	<div class="nav-pd">
+		<div class="nav-pd1">
+			<ul type="none">
+				<li><h4>Sell on Kolexia</h4></li>
+				<li>Open a Shop</li>
+			</ul>
+		</div>
+		<div class="nav-pd2">
+			<ul type="none">
+				<li><h4>Join Us</h4></li>
+				<li>Teams</li>
+				<li>Forums</li>
+				<li>Upcoming Events</li>
+			</ul>
+		</div>
+		<div class="nav-pd3">
+			<ul type="none">
+				<li><h4>Get to Know Us</h4></li>
+				<li>About</li>
+				<li>Careers</li>
+				<li>Developers</li>
+			</ul>
+		</div>
+		<div class="nav-pd4">
+			<ul type="none">
+				<li><h4>Follow Us</h4></li>
+				<li>Facebook</li>
+				<li>Twitter</li>
+				<li>Instagram</li>
+			</ul>
+		</div>
+	</div>
+	<table class="tou">
+		<tr>
+			<td id="copy">&copy; 2014 Kolexia, Inc</td>
+			<td>Terms</td>
+			<td>Copyright</td>
+			<td>Help</td>
+		</tr>
+	</table>
+</div>
+</html>
---
+++ application/views/templates/header.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+	<title>Kolexia | <?php echo $title ?></title>
+	<link rel="stylesheet" type="text/css" href="home.css">
+	
+</head>
+<body>
+<div id="pu">
+	<div class= "nav-pu">
+		<h1 class="logo-name">Kolexia</h1>
+		<h5 class="logo-desc">The #1 Social MarketPlace for Indonesian Handmade in Indonesia</h5>
+		<img src="images/logo2.jpg" width="200px" height="100px" style="margin-left:15%">
+	</div>
+</div>
