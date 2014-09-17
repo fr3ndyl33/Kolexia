937f2d1d8d762367d1beced2e9021f3112524025
kampoengboy <mike@aMichaels-MacBook-Air.local>
2014-09-17 13:00:01 +0700

    Fix Problem

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -1,11 +1,4 @@
 <!--Home Page Content-->
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> 64b012c45bd8f37bf06bfabe7c74cf9f26691766
->>>>>>> 0a6acc36c234be8adffd716da47d3599afe32504
 <a href="" class="loginsite"><div id="loginbutton">
 	Login
 </div></a>
@@ -25,22 +18,6 @@
 	</div>
 	<p id="desc-facename" style="float:right; font-size:20px; font-family:Georgia, Verdana, Times New Roman; margin-top:-20%; margin-right:25%">Jessie</p>
 	<p id="desc-faceshop" style="float:right; font-size:20px; font-family:Georgia, Verdana, Times New Roman; margin-top:-16%; margin-right:22.5%">BooBoo Shop</p>
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-=======
-
-<div id="content">
-	<img src="images/sandal.jpg" width="50%" height="65%" style="padding-top:5px" alt="imagesandal" class="sandal" />
-	<div class="circle">
-		<img src="images/face.jpg" width="25%" height="20%" style="float:left; margin-top:-35%; margin-left:60%" class="face" />	
-	</div>
-	<p id="desc-face" style="float:right; font-size:20px; font-family:Georgia, Verdana, Times New Roman; margin-top:-20%; margin-right:25%">Jessie</p>
-	<p id="desc-face" style="float:right; font-size:20px; font-family:Georgia, Verdana, Times New Roman; margin-top:-16%; margin-right:22.5%">BooBoo Shop</p>
->>>>>>> 1b17040c78c06ea1f4b081880328fe2c708f6cae
->>>>>>> 64b012c45bd8f37bf06bfabe7c74cf9f26691766
->>>>>>> 0a6acc36c234be8adffd716da47d3599afe32504
 	<table class="list-item" style="float:right; margin-top:-10%; margin-right:120px">
 		<tr>
 			<td class="list1"><img src="images/1.jpg" width="100px" height="100px"></td>
