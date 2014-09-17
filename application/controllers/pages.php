471b56b1658d23ce4ce9aa0b3c9eb1226db3f604
kampoengboy <mike@aMichaels-MacBook-Air.local>
2014-09-17 12:39:17 +0700

    Add Login, Register, Home Page, Shopping Cart Design

--- application/controllers/pages.php
+++ application/controllers/pages.php
@@ -1,7 +1,7 @@
 <?php
 
-class Welcome extends CI_Controller {
-	public function index($page='home')
+class Pages extends CI_Controller {
+	public function view($page='home')
 	{
 			if ( ! file_exists(APPPATH.'/views/pages/'.$page.'.php'))
 			{
---
+++ application/views/pages/home.php
@@ -0,0 +1,29 @@
+<!--Home Page Content-->
+<a href="" class="loginsite"><div id="loginbutton">
+	Login
+</div></a>
+
+<a href="" class="registersite"><p id="registerbutton">
+	Register
+</p></a>
+
+<a href="" class="cartsite"><div id="shopcart">
+	<img src="images/cart.png" width="35px" height="35px" style="padding-left:20px; padding-top:30px">
+</div></a>
+
+<div id="content">
+	<img src="images/sandal.jpg" width="50%" height="65%" alt="imagesandal" class="sandal" />
+	<div class="circle">
+		<img src="images/face.jpg" width="25%" height="20%" style="float:left; margin-top:-35%; margin-left:60%" class="face" />	
+	</div>
+	<p id="desc-facename" style="float:right; font-size:20px; font-family:Georgia, Verdana, Times New Roman; margin-top:-20%; margin-right:25%">Jessie</p>
+	<p id="desc-faceshop" style="float:right; font-size:20px; font-family:Georgia, Verdana, Times New Roman; margin-top:-16%; margin-right:22.5%">BooBoo Shop</p>
+	<table class="list-item" style="float:right; margin-top:-10%; margin-right:120px">
+		<tr>
+			<td class="list1"><img src="images/1.jpg" width="100px" height="100px"></td>
+			<td class="list2"><img src="images/2.jpg" width="100px" height="100px"></td>
+			<td class="list3"><img src="images/3.jpg" width="100px" height="100px"></td>
+			<td class="list4"><img src="images/4.jpg" width="100px" height="100px"></td>
+		</tr>
+	</table>
+</div>
